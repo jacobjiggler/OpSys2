e5eadec107847a8191a9242a88096c3aab49abab
Jake Martin <jmart.alb@gmail.com>
2015-03-11 18:36:53 -0400

    A few includes we'll need later

--- Process.py
+++ Process.py
@@ -2,7 +2,7 @@
 #Satoshi Matsuura
 
 
-import rand, Queue
+import rand, Queue, sys, copy
 
 # Time for context switching
 tcs = 4
@@ -39,7 +39,7 @@ if __name__ == '__main__':
 	num_proc = 12
 	num_cpu = 4
 	processes = []
-	
+
 	#arg[0] is the file, arg[1] is optional num_proc, arg[2] is optional numuber of cpu
 	if len(sys.argv) != 1:
 		if len(sys.argv) > 1 and len(sys.argv) <= 3:
@@ -47,7 +47,3 @@ if __name__ == '__main__':
 				num_proc = int(sys.argv[1])
 			if sys.argv[2]:
 				num_cpu = int(sys.argv[2])
-	
-	
-			
-		
