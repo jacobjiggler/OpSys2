b0522c08384017ed14f79d8446ff088c08b5db79
matsus2 <satoshi222@hotmail.com>
2015-03-11 18:16:56 -0400

    The beginning

--- Process.py
+++ Process.py
@@ -1,7 +1,13 @@
+#Jacob Martin
+#Satoshi Matsuura
+
+
 import rand, Queue
 
 # Time for context switching
 tcs = 4
+#global time
+time = 0
 
 
 readyQueue = Queue.Queue()
@@ -28,3 +34,20 @@ class Process:
             return rand.randint(20, 200)
         elif(self.processType == "cpuBound"):
             return rand.randint(1000, 4500)
+
+if __name__ == '__main__':
+	num_proc = 12
+	num_cpu = 4
+	processes = []
+	
+	#arg[0] is the file, arg[1] is optional num_proc, arg[2] is optional numuber of cpu
+	if len(sys.argv) != 1:
+		if len(sys.argv) > 1 and len(sys.argv) <= 3:
+			if sys.argv[1]:
+				num_proc = int(sys.argv[1])
+			if sys.argv[2]:
+				num_cpu = int(sys.argv[2])
+	
+	
+			
+		
