bfbf00243362c6be69db64bf9c550374aebde12c
Jake Martin <jmart.alb@gmail.com>
2015-03-13 18:22:28 -0400

    harglblarg. Good commit message right here

--- hw2.py
+++ hw2.py
@@ -15,10 +15,10 @@ class Process:
         self.remainingBursts = 1
         self.waitTime = 0
         self.pid = pid
-        
+
         if self.processType == "cpuBound":
             self.remainingBursts = 6
-        
+
 
     def setBurstTime(self):
         # Sets random burst time for cpu bound or interactive
@@ -33,18 +33,33 @@ def context_switch(processA, processB, time):
     print "[time " + str(time) + "ms] Context switch (swapping out Process ID " + str(processA.pid) + " for Process ID " + str(processB.pid) +")"
     time+=tcs
     return time
-    
-def FCFS(readyQueue):
-    
+
+def FCFS(readyQueue, num_cpu):
+    temp = 0
+    cpu_num = 0
+    cpu = []
+    num_process = len(readyQueue)
+    cpu = [None for i in range(0,num_cpu)]
+    num_finished = 0
+    for i in range(0, num_cpu):
+        if (len(readyQueue) > 0):
+            cpu[i] = readyQueue.pop(0)
+    while(len(readyQueue) > 0 and num_finished > num_process):
+        if (temp > 0):
+            context_switch(temp_process, readyQueue.pop(0))
+        temp= 1
+        time+= 1
+        cpu_num++;
+    adsf
     return 0
 
-def SJF(readyQueue):
+def SJF(readyQueue, num_cpu):
     return 0
 
-def SJF_preemption(readyQueue):
+def SJF_preemption(readyQueue, num_cpu):
     return 0
 
-def Round_Robin(readyQueue, tcs):
+def Round_Robin(readyQueue, tslice, num_cpu):
     return 0
 
 
@@ -54,13 +69,12 @@ if __name__ == '__main__':
     tSlice = 80
     processes = []
     readyQueue = []
-    cpu = []
     turnaround = []
     total_wait_time = []
-    
 
-    #arg[0] is the file, 
-    #arg[1] is optional num_proc, 
+
+    #arg[0] is the file,
+    #arg[1] is optional num_proc,
     #arg[2] is optional numuber of cpu
     #arg[3] is optional time slice for RR
     if len(sys.argv) != 1:
@@ -73,27 +87,24 @@ if __name__ == '__main__':
         if len(sys.argv) > 1 and len(sys.argv) >= 4:
             if sys.argv[3]:
                 tSlice = int(sys.argv[3])
-                
-    
-    cpu = [None for i in range(0,num_cpu)]
+
+
+
     for i in range(1,num_proc+1):
         if i <= (num_proc * 4/5):
             processes.append(Process("interactive",i))
         else:
             processes.append(Process("cpuBound",i))
-    
-    random.shuffle(processes)
+
     for p in processes:
         readyQueue.append(p)
         if(p.processType == "interactive"):
             print "[time " + str(time) + "ms] Interactive process ID " + str(p.pid) + " entered ready queue (requires " + str(p.burstTime) +  "ms CPU time)"
         else:
             print "[time " + str(time) + "ms] CPU-bound process ID " + str(p.pid) + " entered ready queue (requires " + str(p.burstTime) + "ms CPU time)"
-    
-    
-    #time = context_switch(readyQueue[0], readyQueue[1], time)
-    
-    
-    print "Yay "
-    
 
+
+    #time = context_switch(readyQueue[0], readyQueue[1], time)
+
+
+    print "Yay "
